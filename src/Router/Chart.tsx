bc4dd4bb9be869973d5c39393f4bc18c84b6dadb
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-07-17 10:12:38 +0900

    API 이용 제한으로 인한 차트 기능 삭제

--- src/Router/Chart.tsx
+++ src/Router/Chart.tsx
@@ -1,39 +1,48 @@
-import { useQuery } from "react-query";
-import { fetchOHLCV } from "../api";
+// import { useQuery } from "react-query";
+// import { fetchOHLCV } from "../api";
 import styled from "styled-components";
-import ApexChart from "react-apexcharts";
+// import ApexCharts from "apexcharts";
+// import ApexChart from "react-apexcharts";
 
 interface iChart {
   coinId?: string;
 }
 
-interface iOHLCV {
-  close: number;
-  high: number;
-  low: number;
-  market_cap: number;
-  open: number;
-  time_close: string;
-  time_open: string;
-  volume: number;
-}
+// interface iOHLCV {
+//   close: number;
+//   high: number;
+//   low: number;
+//   market_cap: number;
+//   open: number;
+//   time_close: string;
+//   time_open: string;
+//   volume: number;
+// }
 
-const Loader = styled.div`
+// const Loader = styled.div`
+//   text-align: center;
+//   margin: 30px 0;
+//   font-size: 18px;
+// `;
+
+const Title = styled.h1`
+  font-size: 22px;
+  padding: 24px;
   text-align: center;
-  margin: 30px 0;
-  font-size: 18px;
+  color: gray;
 `;
 
 function Chart({ coinId }: iChart) {
-  const { isLoading, data } = useQuery<iOHLCV[]>([coinId, "OHLCV"], () =>
-    fetchOHLCV(coinId)
-  );
+  // const { isLoading, data } = useQuery<iOHLCV[]>([coinId, "OHLCV"], () =>
+  //   fetchOHLCV(coinId)
+  // );
   return (
     <>
-      {isLoading ? (
+      <Title>Chart data is now blocked by API Provider.</Title>
+      {/* {isLoading ? (
         <Loader>Loading Charts...</Loader>
       ) : (
-        <ApexChart
+        <ApexCharts
           type="candlestick"
           series={[
             {
@@ -67,7 +76,7 @@ function Chart({ coinId }: iChart) {
             },
           }}
         />
-      )}
+      )} */}
     </>
   );
 }
